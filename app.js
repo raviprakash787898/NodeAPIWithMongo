bafa420f4c77d12a17ade3fe64e6a3b9bd716a46
raviprakash787898 <raviprakash787898@gmail.com>
2020-11-22 15:22:29 +0530

    cors changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require("express-async-errors");
 const app = express();
+const cors = require("cors");
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
