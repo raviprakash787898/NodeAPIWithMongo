ee8c3df5f1cea904bb69e6c09f9414ce99b66462
Ravi Prakash <raviprakash787898@gmail.com>
2023-09-10 14:02:23 +0530

    Update connectServer.js
    Updated cred

--- connectServer.js
+++ connectServer.js
@@ -5,8 +5,7 @@ const mongoDBErrors = require("mongoose-mongodb-errors");
 
 require('dotenv').config();
 
-// const mongo_URI = "mongodb+srv://ravi_rp:Ravirp@787898@realmcluster.5y7pm.mongodb.net/Mowgli?retryWrites=true&w=majority";
-const mongo_URI = "mongodb+srv://mfsiraviprakash:<password>@azureclustertest.chctgv2.mongodb.net/AzureClusterTest?retryWrites=true&w=majority";
+const mongo_URI = "mongodb+srv://test-user:bwEbCBwka1CE1aum@azureclustertest.chctgv2.mongodb.net/AzureClusterTestDB?retryWrites=true&w=majority";
 
 mongoose.Promise = global.Promise;
 mongoose.plugin(mongoDBErrors);
